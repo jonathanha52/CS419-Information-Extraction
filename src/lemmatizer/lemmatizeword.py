0cf32889cbe6ee305f6ec0a16489f0a43d178b23
AkallHuynh <huynhluanhkhoacm@gmail.com>
2021-10-09 19:05:15 +0700

    upload lemmatizer

--- src/lemmatizer/lemmatizeword.py
+++ src/lemmatizer/lemmatizeword.py
@@ -5,30 +5,32 @@ from nltk.stem import WordNetLemmatizer
 # nltk.download('wordnet')
 wordnet_lemmatizer = WordNetLemmatizer()
 
-#lemmatizer_type = "wordnet" -> WordNetlemmatizer, "textblob" -> TextBlobLemmatizer
-#pos = "a" -> adj, "n" -> noun, "v" -> verb
-def lemmatize(list_word, lemmatizer_type, pos):
-  result = []
-  if lemmatizer_type == "wordnet" and pos == "a":
-    for word in list_word:
-      result.append(wordnet_lemmatizer.lemmatize(word,pos="a"))
-  else if lemmatizer_type == "wordnet" and pos == "n":
-    for word in list_word:
-      result.append(wordnet_lemmatizer.lemmatize(word, pos="n"))
-  else if lemmatizer_type == "wordnet" and pos == "v":
-    for word in list_word:
-      result.append(wordnet_lemmatizer.lemmatize(word, pos="v"))
+# lemmatizer_type = "wordnet" -> WordNetlemmatizer, "textblob" -> TextBlobLemmatizer
+# pos = "a" -> adj, "n" -> noun, "v" -> verb
 
-  else if lemmatizer_type == "textblob" and pos == "a":
-    for word in list_word:
-      w = Word(word)
-      result.append(w.lemmatize("a"))
-  else if lemmatizer_type == "textblob" and pos == "v":
-    for word in list_word:
-      w = Word(word)
-      result.append(w.lemmatize("v"))
-  else if lemmatizer_type == "textblob" and pos == "n":
-    for word in list_word:
-      w = Word(word)
-      result.append(w.lemmatize("n"))
-  return result
+
+def lemmatize(list_word, lemmatizer_type, pos):
+    result = []
+    if lemmatizer_type == "wordnet" and pos == "a":
+        for word in list_word:
+            result.append(wordnet_lemmatizer.lemmatize(word, pos="a"))
+    elif lemmatizer_type == "wordnet" and pos == "n":
+        for word in list_word:
+            result.append(wordnet_lemmatizer.lemmatize(word, pos="n"))
+    elif lemmatizer_type == "wordnet" and pos == "v":
+        for word in list_word:
+            result.append(wordnet_lemmatizer.lemmatize(word, pos="v"))
+
+    elif lemmatizer_type == "textblob" and pos == "a":
+        for word in list_word:
+            w = Word(word)
+            result.append(w.lemmatize("a"))
+    elif lemmatizer_type == "textblob" and pos == "v":
+        for word in list_word:
+            w = Word(word)
+            result.append(w.lemmatize("v"))
+    elif lemmatizer_type == "textblob" and pos == "n":
+        for word in list_word:
+            w = Word(word)
+            result.append(w.lemmatize("n"))
+    return result
