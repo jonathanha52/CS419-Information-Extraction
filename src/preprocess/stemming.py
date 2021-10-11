86cbb2335ba89c1abebad995239f578f110d0132
jonathanha52 <44718668+jonathanha52@users.noreply.github.com>
2021-10-11 23:49:27 +0700

    Benchmark tfidf (#4)
    * Remove cache folder
    
    * Finish TFIDF module, added benchmarking for 3 types of stemming
    
    * Added requirements

--- src/preprocess/stemming.py
+++ src/preprocess/stemming.py
@@ -1,2 +1,11 @@
 def stemming_document(stemmer, document):
-    return
+    """
+    Function for tokenized document, only work with
+    single word tokenization in current state of development
+    --------------------------------------------
+    Parameters
+    stemmer: Stemmer from NLTK library.
+    document: list
+        List of tokens from a document
+    """
+    return [stemmer.stem(word) for word in document]
--- src/preprocess/tokenize.py
+++ src/preprocess/tokenize.py
@@ -1,3 +1,4 @@
-def splitFile(file_content):
+import string
+def tokenize_string(file_content):
   word_list = file_content.lower().translate(str.maketrans('', '', string.punctuation)).split()
   return word_list
