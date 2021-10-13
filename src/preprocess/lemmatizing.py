a4ba2f2f1b0ac79b8625125a588b2f548fb5e831
jonathanha52 <44718668+jonathanha52@users.noreply.github.com>
2021-10-13 09:35:28 +0700

    Update pos tagging for lemmatizer (#6)
    * Update pos tagging for lemmatizer
    
    * Update lemmatizer for sentence
    
    Co-authored-by: AkallHuynh <huynhluanhkhoacm@gmail.com>

--- src/preprocess/lemmatizing.py
+++ src/preprocess/lemmatizing.py
@@ -1,37 +1,45 @@
-from textblob import Word
+from textblob import TextBlob
 
+from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
-# use this line to download nltk.wordnet
+import nltk
+# use these line to download nltk data
+# nltk.download('tagsets')
 # nltk.download('wordnet')
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
 
 
-# lemmatizer_type = "wordnet" -> WordNetlemmatizer, "textblob" -> TextBlobLemmatizer
-# pos = "a" -> adj, "n" -> noun, "v" -> verb
+def get_wordnet_pos(sentence):
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+    word_and_tag = ([(word, tag_dict.get(nltk.pos_tag([word])[0][1][0].upper(), wordnet.NOUN))
+                    for word in nltk.word_tokenize(sentence)])
+    return word_and_tag
 
 
-def lemmatize(list_word, lemmatizer_type, pos):
+def get_textblob_pos(sentence):
+    tokenized = TextBlob(sentence)
+    tag_dict = {"J": 'a',
+                "N": 'n',
+                "V": 'v',
+                "R": 'r'}
+    word_and_tag = ([(word, tag_dict.get(pos[0], 'n'))
+                    for word, pos in tokenized.tags])
+    return word_and_tag
+
+
+def lemmatize(sentence, lemmatizer_type):
+    # lemmatizer_type = "wordnet" -> WordNetlemmatizer, "textblob" -> TextBlobLemmatizer
     wordnet_lemmatizer = WordNetLemmatizer()
     result = []
-    if lemmatizer_type == "wordnet" and pos == "a":
-        for word in list_word:
-            result.append(wordnet_lemmatizer.lemmatize(word, pos="a"))
-    elif lemmatizer_type == "wordnet" and pos == "n":
-        for word in list_word:
-            result.append(wordnet_lemmatizer.lemmatize(word, pos="n"))
-    elif lemmatizer_type == "wordnet" and pos == "v":
-        for word in list_word:
-            result.append(wordnet_lemmatizer.lemmatize(word, pos="v"))
+    if lemmatizer_type == "wordnet":
+        result = ([wordnet_lemmatizer.lemmatize(
+            word, tag) for word, tag in get_wordnet_pos(sentence)])
 
-    elif lemmatizer_type == "textblob" and pos == "a":
-        for word in list_word:
-            w = Word(word)
-            result.append(w.lemmatize("a"))
-    elif lemmatizer_type == "textblob" and pos == "v":
-        for word in list_word:
-            w = Word(word)
-            result.append(w.lemmatize("v"))
-    elif lemmatizer_type == "textblob" and pos == "n":
-        for word in list_word:
-            w = Word(word)
-            result.append(w.lemmatize("n"))
+    elif lemmatizer_type == "textblob":
+        result = ([word.lemmatize(tag)
+                  for word, tag in get_textblob_pos(sentence)])
     return result
